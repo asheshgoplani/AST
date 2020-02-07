86ba71a1e595efdc7cbec5546c46d3203c80a2b7
asheshgoplani <36273585+asheshgoplani@users.noreply.github.com>
2020-02-07 07:45:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ var esprima = require("esprima");
 var escodegen = require("escodegen");
 
 // Declare the program
-var program = 'console.log("ashesh")';
+var program = `
+
+
+
+console.log("ashesh"); 
+
+//spacing
+
+console.log("yasir");
+
+
+`;
 
 //Parsing the code with esprima which will give us the AST
 var parsedCode = esprima.parseScript(program);
+
+console.log(JSON.stringify(parsedCode))
 
 //Manuplating that AST to change the name
 parsedCode.body[0].expression.arguments[0].value = "yasir";
@@ -13,5 +26,7 @@ parsedCode.body[0].expression.arguments[0].value = "yasir";
 //Generating JS code with escodegen from the manuplated AST
 var generateJs = escodegen.generate(parsedCode);
 
-//running the generated js code
+
+
+console.log(generateJs);
 eval(generateJs);
